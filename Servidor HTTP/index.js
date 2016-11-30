80185d625d42b377f4c82967e8a40311079a9794
Andrés Jácome <andresjacome.j@outlook.com>
2016-11-30 14:41:48 -0500

    get - enviar parametro en la url

--- Servidor HTTP/index.js
+++ Servidor HTTP/index.js
@@ -3,11 +3,46 @@ var express = require('express');
 
 //ejecutar la funcion express
 var app = express();
-var puerto = 5050;
+var puerto = 6060;
+
+var usuarios = [
+    {
+        id:1,
+        nombre:'Pedro',
+        cedula:'12345457'
+    },
+    {
+        id:2,
+        nombre:'Carlos',
+        cedula:'15974534'
+    },
+    {
+        id:3,
+        nombre:'Juan',
+        cedula:'14785236'
+    }
+]
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/Usuario', function (req, res) {
+    res.json(usuarios);
+});
+
+app.get('/Usuario/:idUsuario', function (req, res) {
+    
+    var idActual = req.params.idUsuario;
+    for(var i=0;i<usuarios.length;i++){
+        if(idActual == usuarios[i].id){
+            res.json(usuarios[i]);
+        }
+    }
+    res.send('No existe el Usuario');
+    
+})
 
 app.get('/TecnologiasWeb', function (req, res) {
   res.send('con javascript!');
@@ -15,13 +50,17 @@ app.get('/TecnologiasWeb', function (req, res) {
 
 app.post('/TecnologiasWeb', function (req, res) {
   
+    var parametros = req.params;
+    
+    console.log(parametros);
+    
     var usuario = {
         nombre: 'Deidamia',
         cedula: '0000000000'
     }
     
     //agregar un campo al objeto
-    usuario.apellido = 'Quishpe';
+    /*usuario.apellido = 'Quishpe';
     usuario.mascotas = [];
     usuario.casado = false;
     
@@ -33,7 +72,7 @@ app.post('/TecnologiasWeb', function (req, res) {
     console.log("Cabeceras request");
     console.log(req.headers);
     console.log("Cabeceras response");
-    console.log(res.headers);
+    console.log(res.headers);*/
         
     res.append('token', '1234');
     
