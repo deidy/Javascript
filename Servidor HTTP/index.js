878a2a3c6b0f466f21a4b4b056497633498aeb87
Andrés Jácome <andresjacome.j@outlook.com>
2016-11-30 14:02:59 -0500

    objeto json

--- Servidor HTTP/index.js
+++ Servidor HTTP/index.js
@@ -1,10 +1,45 @@
+//importar el paquete express
 var express = require('express');
-var app = express();
 
-app.get('/TecnologiasWeb', function (req, res) {
+//ejecutar la funcion express
+var app = express();
+var puerto = 5050;
+
+app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(5050, function () {
-  console.log('con javascript!');
+app.get('/TecnologiasWeb', function (req, res) {
+  res.send('con javascript!');
+});
+
+app.post('/TecnologiasWeb', function (req, res) {
+  
+    var usuario = {
+        nombre: 'Deidamia',
+        cedula: '0000000000'
+    }
+    
+    
+    console.log("Lo que tengo en el request");
+    console.log(req);
+    console.log("Lo que tengo en el response");
+    console.log(res);
+    
+    console.log("Cabeceras request");
+    console.log(req.headers);
+    console.log("Cabeceras response");
+    console.log(res.headers);
+    
+    //enviar objetos json
+    res.json(usuario);
+    
+    res.send('con POST!');
+    
+    //no se puede enviar dos respuestas
+    //res.send('asas!');
+});
+
+app.listen(puerto, function () {
+  console.log('Example app listening on port ' + puerto + '!');
 });
