0d6a38b7dff60951b4233032b3f02ad2b5368f6b
aaa <aa@aa.com>
2016-12-20 14:33:15 -0500

    añadir vistas y configurar rutas

--- Servidor HTTP/Mascotas/config/routes.js
+++ Servidor HTTP/Mascotas/config/routes.js
@@ -34,6 +34,18 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
+  },
+    
+  '/Quito':{
+      view: 'quito'
+  },
+    
+  '/Guayaquil':{
+      view: 'guayaquil'
+  },
+    
+  '/Cuenca':{
+      view: 'cuenca'
   }
 
   /***************************************************************************
