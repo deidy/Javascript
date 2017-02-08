ffae2ce4413129397bf2033b87983c8cb2aa710d
aaa <aa@aa.com>
2017-02-08 14:58:53 -0500

    editar usuario

--- NuevoServidor/Mascotas/api/controllers/UsuarioController.js
+++ NuevoServidor/Mascotas/api/controllers/UsuarioController.js
@@ -4,138 +4,190 @@
  * @description :: Server-side logic for managing Usuarios
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
-
-
-
 module.exports = {
-
+    
     crearUsuario: function (req, res) {
+        
         //   Se accede asi: /Usuario/crearUsuario
-
         // Guardando todos los parametros en la variable parametros
-
         if (req.method == "POST") {
-
+            
             var parametros = req.allParams();
-
+            
             if (parametros.nombres && parametros.apellidos) {
-
+                
                 var usuarioCrear = {
-                    nombres: parametros.nombres,
-                    apellidos: parametros.apellidos,
-                    correo: parametros.correo
+                    nombres: parametros.nombres
+                    , apellidos: parametros.apellidos
+                    , correo: parametros.correo
                 }
                 
                 if (parametros.correo == "") {
                     delete usuarioCrear.correo;
                 }
-
+                
                 //Todos los métodos son con el modelo
                 Usuario.create(usuarioCrear).exec(function (err, usuarioCreado) {
-
                     if (err) {
                         return res.view('vistas/Error', {
                             error: {
-                                descripcion: "Fallo al crear un Usuario",
-                                rawError: err,
-                                url: "/crearUsuario"
+                                descripcion: "Fallo al crear un Usuario"
+                                , rawError: err
+                                , url: "/crearUsuario"
                             }
                         });
                     }
                     
-                    Usuario.find()
-            .exec(function(errorIndefinido, usuariosEncontrados){
-            
-            if(errorIndefinido){
-                res.view('vistas/Error', {
-                    error:{
-                        descripcion: "Hubo un problema cargando los usuarios",
-                        rawError: errorIndefinido,
-                        url: "/ListarUsuarios"
-                    }
-                });
-            }
-            
-            res.view('vistas/Usuario/ListarUsuarios', {
-                usuarios: usuariosEncontrados
-            });
-            
-        })
-
+                    Usuario.find().exec(function (errorIndefinido, usuariosEncontrados) {
+                        
+                        if (errorIndefinido) {
+                            res.view('vistas/Error', {
+                                error: {
+                                    descripcion: "Hubo un problema cargando los usuarios"
+                                    , rawError: errorIndefinido
+                                    , url: "/ListarUsuarios"
+                                }
+                            });
+                        }
+                        
+                        res.view('vistas/Usuario/ListarUsuarios', {
+                            usuarios: usuariosEncontrados
+                        });
+                    })
                 })
-
-            } else {
+            }
+            else {
                 return res.view('vistas/Error', {
                     error: {
-                        descripcion: "Llena todos los parámetros, nombres y apellidos",
-                        rawError: "Fallo en envío de parámetros",
-                        url: "/crearUsuario"
+                        descripcion: "Llena todos los parámetros, nombres y apellidos"
+                        , rawError: "Fallo en envío de parámetros"
+                        , url: "/crearUsuario"
                     }
                 });
             }
-
-        } else {
+        }
+        else {
             return res.view('vistas/Error', {
                 error: {
-                    descripcion: "Error en el uso del Método HTTP",
-                    rawError: "HTTP Inválido",
-                    url: "/crearUsuario"
+                    descripcion: "Error en el uso del Método HTTP"
+                    , rawError: "HTTP Inválido"
+                    , url: "/crearUsuario"
                 }
             });
         }
-
     },
     
-    borrarUsuario: function (req, res){
+    borrarUsuario: function (req, res) {
         
         var parametros = req.allParams();
         
-        if(parametros.id){
+        if (parametros.id) {
             
             Usuario.destroy({
                 id: parametros.id
-            }).exec(function (errorInesperado, usuarioRemovido){
+            }).exec(function (errorInesperado, usuarioRemovido) {
                 
-                if(errorInesperado) {
+                if (errorInesperado) {
                     return res.view('vistas/Error', {
-                            error: {
-                                descripcion: "Tuvimos un Error Inesperado",
-                                rawError: errorInesperado,
-                                url: "/ListarUsuarios"
-                            }
-                        });
+                        error: {
+                            descripcion: "Tuvimos un Error Inesperado"
+                            , rawError: errorInesperado
+                            , url: "/ListarUsuarios"
+                        }
+                    });
                 }
                 
-                Usuario.find()
-            .exec(function(err, usuariosEncontrados){
-            
-            if(err){
-                res.view('vistas/Error', {
-                    error:{
-                        descripcion: "Hubo un problema cargando los usuarios",
-                        rawError: error,
-                        url: "/ListarUsuarios"
+                Usuario.find().exec(function (err, usuariosEncontrados) {
+                    
+                    if (err) {
+                        res.view('vistas/Error', {
+                            error: {
+                                descripcion: "Hubo un problema cargando los usuarios"
+                                , rawError: error
+                                , url: "/ListarUsuarios"
+                            }
+                        });
                     }
-                });
-            }
-            
-            res.view('vistas/Usuario/ListarUsuarios', {
-                usuarios: usuariosEncontrados
-            });
-            
-        })
-                
+                    res.view('vistas/Usuario/ListarUsuarios', {
+                        usuarios: usuariosEncontrados
+                    });
+                })
             })
-            
-        }else{
+        }
+        else {
             return res.view('vistas/Error', {
                 error: {
-                    descripcion: "Necesitamos el id para borrar al Usuario",
-                    rawError: "No envía ID",
-                    url: "/ListarUsuarios"
+                    descripcion: "Necesitamos el id para borrar al Usuario"
+                    , rawError: "No envía ID"
+                    , url: "/ListarUsuarios"
+                }
+            });
+        }
+    },
+    
+    editarUsuario: function (req, res) {
+        
+        var parametros = req.allParams();
+        
+        if (parametros.id && (parametros.nombres || parametros.apellidos || parametros.correo)) {
+                    
+            var usuarioEditar = {
+                nombres: parametros.nombres,
+                apellidos: parametros.apellidos,
+                correo: parametros.correo
+            }
+            
+            if(usuarioEditar.nombres = ""){
+                delete usuarioEditar.nombres
+            }
+            
+            if(usuarioEditar.apellidos = ""){
+                delete usuarioEditar.apellidos
+            }
+            
+            if(usuarioEditar.correo = ""){
+                delete usuarioEditar.correo
+            }
+            
+            Usuario.update({
+                id: parametros.id
+            }).exec(function (errorInesperado, usuarioRemovido) {
+                
+                if (errorInesperado) {
+                    return res.view('vistas/Error', {
+                        error: {
+                            descripcion: "Tuvimos un Error Inesperado"
+                            , rawError: errorInesperado
+                            , url: "/ListarUsuarios"
+                        }
+                    });
+                }
+                
+                Usuario.find().exec(function (err, usuariosEncontrados) {
+                    
+                    if (err) {
+                        res.view('vistas/Error', {
+                            error: {
+                                descripcion: "Hubo un problema cargando los usuarios"
+                                , rawError: error
+                                , url: "/ListarUsuarios"
+                            }
+                        });
+                    }
+                    res.view('vistas/Usuario/ListarUsuarios', {
+                        usuarios: usuariosEncontrados
+                    });
+                })
+            })
+        }
+        else {
+            return res.view('vistas/Error', {
+                error: {
+                    descripcion: "Necesitamos que envíes el ID y el nombre, apellido o correo"
+                    , rawError: "No envía parámetros"
+                    , url: "/EditarUsuarios"
                 }
             });
         }
     }
-    
 };
