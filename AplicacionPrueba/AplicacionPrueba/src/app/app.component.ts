0ab3221728ca809465a7d780a61c870dd9b5d979
a <a>
2017-02-24 12:47:58 -0500

    listar tiendas

--- AplicacionPrueba/AplicacionPrueba/src/app/app.component.ts
+++ AplicacionPrueba/AplicacionPrueba/src/app/app.component.ts
@@ -10,47 +10,29 @@ import {MasterURLService} from "./services/master-url.service";
 })
 // CTRL A +  -  CTRL + ALT + L
 export class AppComponent implements OnInit {
-  title: string = "Hola Amigos";
-  nombre: string = "";
-  apellido: string = "";
-  colorH4 = "red";
-  tamanoH4 = "52px";
-  classes = "btn btn-block btn-success";
-
-  error: string = "No hay errores";
-
-  nuevaTienda: any = {};
+  title: string = "Bienvenido a Ingresar Tiendas";
+  nuevaTienda = {};
+  tiendas = [];
 
   disabledButtons = {
     NuevaTiendaFormSubmitButton: false
   };
 
-
   constructor(private _http: Http,
               private _masterURL: MasterURLService) {
 
-    this.apellido = "Eguez";
-    this.nombre = "Adrian";
-    console.log("Inicio el constructor")
   }
 
   ngOnInit() {
-    this.apellido = "Sarzosa";
-    this.nombre = "Vicente";
-    console.log("On Init")
-  }
-
-  nombreCompleto(): string {
-    return `${this.nombre} ${this.apellido}`
-  }
-
-  hizoClick() {
-    console.log("Hizo Click");
-    console.log()
-  }
-
-  hizoFocus() {
-    console.log("Hizo focus");
+    this._http.get(this._masterURL.url+"Tienda")
+      .subscribe(
+        (res:Response)=>{
+          this.tiendas = res.json();
+        },
+        (err)=>{
+          console.log(err);
+        }
+      )
   }
 
   crearTienda(formulario) {
